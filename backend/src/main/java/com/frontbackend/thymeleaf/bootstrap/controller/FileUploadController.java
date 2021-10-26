03c93935963ca6f91ea1586e99b86c969bb949e3
Ajay Singh <>
2021-10-26 16:17:40 +0530

    Completed backend and frontend

--- backend/src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/FileUploadController.java
+++ backend/src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/FileUploadController.java
@@ -1,13 +1,11 @@
 package com.frontbackend.thymeleaf.bootstrap.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.frontbackend.thymeleaf.bootstrap.model.Upload;
-import com.frontbackend.thymeleaf.bootstrap.repository.UploadRepository;
+import com.amazonaws.services.s3.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +16,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -26,44 +28,46 @@ public class FileUploadController {
 
   private final AmazonS3 amazonS3Client;
 
-  private final UploadRepository uploadRepository;
 
   @Value("${app.awsServices.bucketName}")
   private String bucketName;
 
-  public FileUploadController(AmazonS3 amazonS3Client, UploadRepository uploadRepository) {
+  public FileUploadController(AmazonS3 amazonS3Client) {
     this.amazonS3Client = amazonS3Client;
-    this.uploadRepository = uploadRepository;
   }
 
   @GetMapping
-  public String main() {
+  public String main(Model model) {
+    Map<String, String> statusMap = new HashMap<String, String>();
+    ObjectListing objectListing = amazonS3Client.listObjects(bucketName);
+    for (S3ObjectSummary os : objectListing.getObjectSummaries()) {
+      GetObjectTaggingRequest getTaggingRequest = new GetObjectTaggingRequest(bucketName, os.getKey());
+      GetObjectTaggingResult tags = amazonS3Client.getObjectTagging(getTaggingRequest);
+      Tag tag = tags.getTagSet().get(0);
+      statusMap.put(os.getKey(), tag.getValue());
+    }
+    model.addAttribute("statusMap", statusMap);
     return "index";
   }
 
   @PostMapping
-  public String upload(@RequestParam("emails") String emails, @RequestParam("file") MultipartFile multipartFile, RedirectAttributes redirectAttributes) {
+  public String upload(@RequestParam("file") MultipartFile multipartFile, RedirectAttributes redirectAttributes) {
     File file = convertMultiPartFileToFile(multipartFile);
 
     String uniqueFileName = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
 
-    prepareUploadAndSaveInDatabase(uniqueFileName, emails);
-
     uploadFileToS3bucket(uniqueFileName, file, bucketName);
 
-    redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + multipartFile.getOriginalFilename() + "!");
+    redirectAttributes.addFlashAttribute("message", "File uploaded successfully :  " + multipartFile.getOriginalFilename() + "!");
     return "redirect:/";
   }
 
-  private void prepareUploadAndSaveInDatabase(String uniqueFileName, String emails) {
-    Upload upload = new Upload();
-    upload.setFile_name(uniqueFileName);
-    upload.setEmails(emails);
-    uploadRepository.save(upload);
-  }
-
   private void uploadFileToS3bucket(String fileName, File file, String bucketName) {
+    List<Tag> tags = new ArrayList<>();
+    tags.add(new Tag("status", "PROCESSING"));
+    ObjectTagging objectTag = new ObjectTagging(tags);
     PutObjectRequest request = new PutObjectRequest(bucketName, fileName, file);
+    request.withTagging(objectTag);
     request.withCannedAcl(CannedAccessControlList.PublicRead);
     amazonS3Client.putObject(request);
   }
--- backend/src/main/java/com/frontbackend/thymeleaf/bootstrap/repository/UploadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.frontbackend.thymeleaf.bootstrap.repository;
-
-import com.frontbackend.thymeleaf.bootstrap.model.Upload;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadRepository extends JpaRepository<Upload, Integer> {
-}
